826afe735fda90a40b66cc091d5d63ff100a38f7
toyianto <55838366+toyianto@users.noreply.github.com>
2020-10-25 21:21:33 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,7 @@
 $host  = "localhost";
 $username  = "root";
 $password  = "";
-$database  = "tugas_1";
+$database  = "tugas_uts";
 $koneksi  = mysqli_connect($host,  $username,  $password, $database);
 $pilihdatabase  = mysqli_select_db($koneksi, "stki");
 if  ($pilihdatabase) echo "<h3>Koneksi Berhasil </h3><hr><br/>";
